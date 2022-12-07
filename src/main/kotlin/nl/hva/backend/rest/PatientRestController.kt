fb2f0b8ea4aca74845655dfaab51d1fdf8697179
meierm <matthias.meier@hva.nl>
2022-12-07 13:21:00 +0100

    rest API to get patient id

--- src/main/kotlin/nl/hva/backend/rest/PatientRestController.kt
+++ src/main/kotlin/nl/hva/backend/rest/PatientRestController.kt
@@ -2,8 +2,10 @@ package nl.hva.backend.rest
 
 import nl.hva.backend.application.api.GeneralPractitionerService
 import nl.hva.backend.application.api.PatientService
+import nl.hva.backend.application.dto.GeneralPractitionerDTO
 import nl.hva.backend.application.dto.PatientDTO
 import nl.hva.backend.domain.ids.GeneralPractitionerId
+import nl.hva.backend.domain.ids.PatientId
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
@@ -20,6 +22,19 @@ class PatientRestController {
     @GetMapping("/getAll")
     fun getAll(): List<PatientDTO> {
         return this.patientService.getAllAccounts()
+    }
+
+    @GetMapping("/getById/{id}")
+    @ResponseBody
+    fun getAccountById(
+        @PathVariable("id") id: String
+    ): PatientDTO? {
+        // TODO: return something else than null (maybe ResponseEntity<>)
+        return try {
+            this.patientService.getAccountById(PatientId(id))
+        } catch (e: Exception) {
+            null
+        }
     }
 
     @GetMapping("/getByGeneralPractitionerId/{id}")
